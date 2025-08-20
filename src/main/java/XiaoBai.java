d25eff891595a3abef07d7bb92c414ea4a44b500
Yuewei <caoyueweiwei@126.com>
2025-08-20 14:48:09 +0800

    Level-1: Echo

--- src/main/java/XiaoBai.java
+++ src/main/java/XiaoBai.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class XiaoBai {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo_text = "__  __ ___    _    ___   ____    _    ___ \n"
                 + "\\ \\/ /|_ _|  / \\  / _ \\ | __ )  / \\  |_ _|\n"
                 + " \\  /  | |  / _ \\| | | ||  _ \\ / _ \\  | | \n"
@@ -12,7 +16,22 @@ public class XiaoBai {
         System.out.println(" Hello! I'm XiaoBai, your dragon assistant.");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.trim().equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
