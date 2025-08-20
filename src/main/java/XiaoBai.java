ff813b42a23219d3eef44a23d4b1b090a5abe3af
Yuewei <caoyueweiwei@126.com>
2025-08-20 15:08:51 +0800

    Level-3: Mark as Done

--- src/main/java/XiaoBai.java
+++ src/main/java/XiaoBai.java
@@ -4,14 +4,17 @@ public class XiaoBai {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         String logo_text = "__  __ ___    _    ___   ____    _    ___ \n"
-                + "\\ \\/ /|_ _|  / \\  / _ \\ | __ )  / \\  |_ _|\n"
-                + " \\  /  | |  / _ \\| | | ||  _ \\ / _ \\  | | \n"
-                + " /  \\  | | / ___ \\ |_| || |_) / ___ \\ | | \n"
-                + "/_/\\_\\|___/_/   \\_\\___/ |____/_/   \\_\\___|\n";
+                        + "\\ \\/ /|_ _|  / \\  / _ \\ | __ )  / \\  |_ _|\n"
+                        + " \\  /  | |  / _ \\| | | ||  _ \\ / _ \\  | | \n"
+                        + " /  \\  | | / ___ \\ |_| || |_) / ___ \\ | | \n"
+                        + "/_/\\_\\|___/_/   \\_\\___/ |____/_/   \\_\\___|\n";
         String expression = "(*^_^*)";
+
+
         System.out.println("____________________________________________________________");
         System.out.println(logo_text);
         System.out.println(expression);
@@ -27,15 +30,60 @@ public class XiaoBai {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "[X]" : "[ ]";
+                    System.out.println(" " + (i + 1) + "." + status + " " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                    if (index >= 0 && index < taskCount) {
+                        isDone[index] = true;
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   [X] " + tasks[index]);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Invalid task number.");
+                        System.out.println("____________________________________________________________");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Invalid format. Use: mark [task number]");
+                    System.out.println("____________________________________________________________");
+                }
+
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                    if (index >= 0 && index < taskCount) {
+                        isDone[index] = false;
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   [ ] " + tasks[index]);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Invalid task number.");
+                        System.out.println("____________________________________________________________");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Invalid format. Use: unmark [task number]");
+                    System.out.println("____________________________________________________________");
+                }
+
             } else {
                 if (taskCount < 100) {
                     tasks[taskCount] = input;
+                    isDone[taskCount] = false;
                     taskCount++;
                     System.out.println("____________________________________________________________");
                     System.out.println(" added: " + input);
