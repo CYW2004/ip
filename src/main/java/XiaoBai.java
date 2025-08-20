182457ebb2498f24eea93b44e806a5ed2a58a6f0
Yuewei <caoyueweiwei@126.com>
2025-08-20 14:24:31 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/XiaoBai.java
@@ -0,0 +1,18 @@
+public class XiaoBai {
+    public static void main(String[] args) {
+        String logo_text = "__  __ ___    _    ___   ____    _    ___ \n"
+                + "\\ \\/ /|_ _|  / \\  / _ \\ | __ )  / \\  |_ _|\n"
+                + " \\  /  | |  / _ \\| | | ||  _ \\ / _ \\  | | \n"
+                + " /  \\  | | / ___ \\ |_| || |_) / ___ \\ | | \n"
+                + "/_/\\_\\|___/_/   \\_\\___/ |____/_/   \\_\\___|\n";
+        String expression = "(*^_^*)";
+        System.out.println("____________________________________________________________");
+        System.out.println(logo_text);
+        System.out.println(expression);
+        System.out.println(" Hello! I'm XiaoBai, your dragon assistant.");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
