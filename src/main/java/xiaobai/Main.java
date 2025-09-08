8706e4a2361b32d5f9bd3d335281e836244419f6
Yuewei <caoyueweiwei@126.com>
2025-09-08 12:51:46 +0800

    A-JavaDoc
    I have finished work of this tag already, but I forgot to create this
    tag.

--- src/main/java/xiaobai/Main.java
+++ src/main/java/xiaobai/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
+            AnchorPane ap = fxmlLoader.load() ;
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setTitle("XiaoBai");
