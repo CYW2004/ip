58e46c3e9632de9d10d866dd53f3944182a167a3
Yuewei <caoyueweiwei@126.com>
2025-09-09 14:35:07 +0800

    Level-10: GUI
    I have finished tasks of this level, but forgot to create this branch.

--- src/main/java/xiaobai/Launcher.java
+++ src/main/java/xiaobai/Launcher.java
@@ -2,7 +2,6 @@ package xiaobai;
 
 import javafx.application.Application;
 
-/** Launcher to workaround JavaFX classpath issues. */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
